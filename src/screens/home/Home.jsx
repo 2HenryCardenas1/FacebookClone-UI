4d4f8e29040162565efa3fea746af63b57e3c478
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-07 16:10:30 -0500

    Screen home created

---
+++ src/screens/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        marginTop: 10,
+      }}
+    >
+      <Text>Home sassa</Text>
+    </View>
+  );
+};
+
+export {Home};
