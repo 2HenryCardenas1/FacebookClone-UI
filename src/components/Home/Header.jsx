30a0992bedcbcd68abf9dcea8ffaf37b7f4c5e9f
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-08 12:36:42 -0500

    component create status  created

---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,60 @@
+import {Avatar, Icon, Input} from "@rneui/themed";
+import React from "react";
+import {View} from "react-native";
+
+const Header = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+
+        paddingVertical: 5,
+      }}
+    >
+      <Avatar
+        source={require("../../assets/images/imageProfile.jpg")}
+        containerStyle={{
+          width: 50,
+          height: 50,
+        }}
+        avatarStyle={{
+          width: 50,
+          height: 50,
+          borderRadius: 50,
+          borderColor: "#F0F2F5",
+          borderWidth: 1,
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Input
+          placeholder="What you are thinking?"
+          style={{
+            backgroundColor: "#F0F2F5",
+            borderRadius: 20,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            fontSize: 15,
+            height: 50,
+          }}
+          inputContainerStyle={{
+            borderColor: "transparent",
+          }}
+          containerStyle={{
+            height: 50,
+          }}
+          multiline={true}
+        />
+      </View>
+
+      <Icon name="camera" type="ionicon" color="#1877F2" size={30} />
+    </View>
+  );
+};
+
+export {Header};
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import {Text, View} from "react-native";
-
+import {Header} from "../../components/Home/Header";
 const Home = () => {
   return (
     <View
       style={{
-        marginTop: 10,
+        flex: 1,
+        backgroundColor: "white",
+        paddingHorizontal: 10,
+        paddingTop: 10,
       }}
     >
+      <Header />
       <Text>Home sassa</Text>
     </View>
   );
