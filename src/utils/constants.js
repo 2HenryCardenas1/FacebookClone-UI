e3659016585eeb4d1969fcdf0a023162ddeb96fb
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-30 15:22:25 -0500

    constants edited

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,13 @@ const HomeScreen = "Home";
 const VideoScreen = "Video";
 
 
+const STYLE_SCROLL_VIEW_SCREEN = {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 5,
+    paddingTop: 10,
+}
 
-export { HomeScreen, VideoScreen };
+
+
+export { HomeScreen, VideoScreen, STYLE_SCROLL_VIEW_SCREEN };
